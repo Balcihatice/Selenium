848c23b6d67dc2a50f3cc6c0b849a0b86b19bc86
Balcihatice <aciceaybike@gmail.com>
2022-12-19 00:01:12 +0300

    Homework

--- src/test/java/day01_drivermethods/Homework1.java
+++ src/test/java/day01_drivermethods/Homework1.java
@@ -1,14 +1,32 @@
 package day01_drivermethods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.DriverManager;
+
 public class Homework1 {
- /*   Youtube ana sayfasina gidelim . https://www.youtube.com/
+ /* Youtube ana sayfasina gidelim . https://www.youtube.com/
     Amazon soyfasina gidelim. https://www.amazon.com/
     Tekrar YouTube’sayfasina donelim
     Yeniden Amazon sayfasina gidelim
+    Sayfa gecıslerınde 3 sanıye bekleyelım
     Sayfayi Refresh(yenile) yapalim
     Sayfayi kapatalim / Tum sayfalari kapatalim
     */
- public static void main(String[] args) {
+ public static void main(String[] args) throws InterruptedException {
+  WebDriver driver = new ChromeDriver();
 
+  driver.navigate().to("https://youtube.com");
+  Thread.sleep(3000);
+  driver.navigate().to("https://amazon.com");
+  Thread.sleep(3000);
+  driver.navigate().back();
+  Thread.sleep(3000);
+  driver.navigate().forward();
+  Thread.sleep(3000);
+
+  driver.navigate().refresh();
+  driver.close();
  }
 }
--- src/test/java/day01_drivermethods/Homework2.java
+++ src/test/java/day01_drivermethods/Homework2.java
@@ -1,11 +1,32 @@
 package day01_drivermethods;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.swing.text.Position;
+
 public class Homework2 {
+
+    //Amazon soyfasina gidelim. https://www.amazon.com/
+    //Sayfanin konumunu ve boyutlarini yazdirin
+    //Sayfanin konumunu ve boyutunu istediginiz sekilde ayarlayin
+    //Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
+    //Sayfayi kapatin
     public static void main(String[] args) {
-        //Amazon soyfasina gidelim. https://www.amazon.com/
-        //Sayfanin konumunu ve boyutlarini yazdirin
-        //Sayfanin konumunu ve boyutunu istediginiz sekilde ayarlayin
-        //Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
-        //Sayfayi kapatin
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.amazon.com");
+
+        System.out.println(driver.manage().window().getPosition());//konum
+        System.out.println(driver.manage().window().getSize());//boyut
+
+
+        driver.manage().window().setPosition(new Point(15, 40));
+        System.out.println(driver.manage().window().getPosition());
+
+        driver.manage().window().setSize(new Dimension(900, 600));
+        System.out.println(driver.manage().window().getSize());
     }
 }

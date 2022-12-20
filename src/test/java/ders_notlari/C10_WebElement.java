b1dd1562cde0321ff91110abf050bb45d2a295d7
Balcihatice <aciceaybike@gmail.com>
2022-12-20 03:10:48 +0300

    Selenium_DersNotlari

---
+++ src/test/java/ders_notlari/C10_WebElement.java
@@ -0,0 +1,18 @@
+package ders_notlari;
+
+public class C10_WebElement {
+
+    /*
+    Web sayfasinda kullanilan etkilesimli yada etkilesimsiz hersey web elementtir.
+    Button
+    Searc box(Arama kutusu)
+    Headers(basliklar)
+    Tables(tablolar)
+    Farkli turde web eement taglari vardir
+    <html>,<body>,<label>,<input>,<a> gibi
+    */
+    public static void main(String[] args) {
+
+
+    }
+}
